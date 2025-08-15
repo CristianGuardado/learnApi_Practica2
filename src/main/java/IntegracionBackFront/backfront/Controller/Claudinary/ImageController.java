c8fd63c7c5039d68c00df02a41f07bcd9f9de999
CristianGuardado <20240365@ricaldone.edu.sv>
2025-08-15 15:51:47 -0600

    Terminado

---
+++ src/main/java/IntegracionBackFront/backfront/Controller/Claudinary/ImageController.java
@@ -0,0 +1,10 @@
+package IntegracionBackFront.backfront.Controller.Claudinary;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/image")
+public class ImageController {
+
+}
